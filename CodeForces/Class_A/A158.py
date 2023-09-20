968cc1d72f7861535aef0d15ffd822ad0c0ff5a7
s.fayzullaev <s.fayzullayev@loc.tashkent.uz>
2023-09-20 15:33:49 +0500

    a158 problem solved

--- CodeForces/Class_A/A158.py
+++ CodeForces/Class_A/A158.py
@@ -1,4 +1,17 @@
 n, k = map(int, input().split())
 scores = list(map(int, input().split()))
+partisipants = 0
 
-
+for i in range(0,n):
+    if scores[i] == 0:
+        print(partisipants)
+        break
+    elif i <= k-1:
+        partisipants += 1
+    elif scores[i] == scores[i-1]:
+        partisipants += 1
+    else:
+        print(partisipants)
+        break
+else:
+    print(partisipants)
