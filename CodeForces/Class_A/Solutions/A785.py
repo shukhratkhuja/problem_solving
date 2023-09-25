01493c108d5fd9f7fb57af4f39b15a58c78c42d9
Shuxratxo'ja Xasanovich Fayzullayev <s.fayzullayev@loc.tashkent.uz>
2023-09-25 14:06:09 +0500

    A785 problem resolved

--- CodeForces/Class_A/Solutions/A785.py
+++ CodeForces/Class_A/Solutions/A785.py
@@ -1,17 +1,26 @@
 n = int(input())
 sides = 0
+shapes = {
+    "Tetrahedron": 4,
+    "Cube": 6,
+    "Octahedron": 8,
+    "Dodecahedron": 12,
+    "Icosahedron": 20
+}
+
 for _ in range(n):
     shape = input()
-    if shape == "Tetrahedron":
-        sides += 4
-    elif shape == "Cube":
-        sides += 6
-    elif shape == "Octahedron":
-        sides += 8
-    elif shape == "Dodecahedron":
-        sides += 12
-    elif shape == "Icosahedron":
-        sides += 20
-    else:
-        print("ERROR")
+    sides += shapes[shape]
+    # if shape == "Tetrahedron":
+    #     sides += 4
+    # elif shape == "Cube":
+    #     sides += 6
+    # elif shape == "Octahedron":
+    #     sides += 8
+    # elif shape == "Dodecahedron":
+    #     sides += 12
+    # elif shape == "Icosahedron":
+    #     sides += 20
+    # else:
+    #     print("ERROR")
 print(sides)
