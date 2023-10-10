8712206ad868beabe69b449d5cfa9b0c2a4b46ff
s.fayzullaev <s.fayzullayev@loc.tashkent.uz>
2023-10-10 14:59:42 +0500

    A1624 problem solved

--- CodeForces/A1624.py
+++ CodeForces/A1624.py
@@ -2,9 +2,5 @@ t = int(input())
 
 for _ in range(t):
     n = int(input())
-    
-    x = (int(str(n)[0])-1) * 10
-    x += len(str(n))*(len(str(n))+1)/2
-    
-    print(int(x))
-
+    l = list(map(int, input().split()))
+    print(max(l)-min(l))
