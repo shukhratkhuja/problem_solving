9dcb355d4c2099c5919ce8b97706a5ff732ecb77
Shuxratxo'ja Xasanovich Fayzullayev <s.fayzullayev@loc.tashkent.uz>
2023-10-13 11:22:54 +0500

    LeetCode: valid_parentheses problem solved

---
+++ LeetCode/valid_parentheses.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def isValid(self, s):
+        d = {
+            "}": "{", 
+            "]": "[", 
+            ")": "(", 
+        }
+        left = []
+
+        for p in s:
+            try:
+                if p in "({[":
+                    left.append(p)
+                elif left[-1] == d[p]:
+                        del left[-1]
+                else: return False
+            except: return False
+
+        if left:
+            return False
+        return True
+
+x = Solution().isValid("([)]")
+print(x)
