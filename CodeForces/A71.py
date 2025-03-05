a540e4c01f3ea6ea48055fe1c2f999fd0639c19d
s.fayzullaev <shukhrat.fayzulla@gmail.com>
2025-03-05 12:13:52 +0500

    A4, 71, 231 problems solved

---
+++ CodeForces/A71.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+def way_too_long_words(word):
+
+    if len(word) > 10:
+        return f"{word[0]}{len(word)-2}{word[-1]}"
+    return word
+
+for i in range(n):
+    word = input()
+    print(way_too_long_words(word=word))
