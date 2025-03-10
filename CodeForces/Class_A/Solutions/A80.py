c90bf6426bcf019d8d5ab8d758e0f5635aa5b99f
s.fayzullaev <shukhrat.fayzulla@gmail.com>
2025-03-10 14:51:31 +0500

    A80 solved

--- CodeForces/Class_A/Solutions/A80.py
+++ CodeForces/Class_A/Solutions/A80.py
@@ -1,12 +1,20 @@
-n = int(input())
-m = int(input())
+n, m = map(int, input().split())
+
 def is_prime(n):
-    for i in range(2,n//2+1):
+    for i in range(2,int(n**(1/2)+1)):
         if n % i == 0:
             return False
     else: return True
-print(is_prime(n))
 
-for i in range(n, m+1):
-    if is_prime(i):
-        pass
+result = -1
+for i in range(n+1, m+1):
+    if i % 2 ==1 and is_prime(i):
+        result = i
+        break
+
+if result == m:
+    print("YES")
+else:
+    print("NO")
+
+        
