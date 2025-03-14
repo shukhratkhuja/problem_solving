a6c8b9727dbdb11b2fbc7e11a1b3f28ba1cbe731
shukhratkhuja <shukhrat.fayzulla@gmail.com>
2025-03-14 10:03:38 +0500

    A1857 solved

---
+++ CodeForces/A1857.py
@@ -0,0 +1,16 @@
+import sys
+
+t = int(sys.stdin.readline().strip())
+results = []
+for _ in range(t):
+
+    n = int(sys.stdin.readline().strip())
+    a = list(map(int, sys.stdin.readline().strip().split()))
+    
+    if sum(a) % 2 == 0:
+        results.append("YES")
+    else:
+        results.append("NO")
+
+
+sys.stdout.write("\n".join(results) + "\n")
