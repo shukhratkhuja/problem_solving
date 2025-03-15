6404359d8f0219f6388663b83af3d386786f466e
s.fayzullaev <shukhrat.fayzulla@gmail.com>
2025-03-15 23:48:54 +0500

    B1742 solved

--- CodeForces/B1742.py
+++ CodeForces/B1742.py
@@ -7,13 +7,7 @@ for _ in range(t):
 
     n = int(sys.stdin.readline().strip())
     a = list(map(int, sys.stdin.readline().strip().split()))
-
-    min_candy = min(a)
-    result = 0
-    for i in a:
-        if i != min_candy:
-            result += i-min_candy
-   
-    results.append(str(result))
+    
+    results.append("YES" if len(a) == len(set(a)) else "NO")
 
 sys.stdout.write("\n".join(results) + "\n")
