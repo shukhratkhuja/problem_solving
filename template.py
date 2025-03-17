69a7ac6bad008809332d8d343e02a080d30df6c0
shukhratkhuja <shukhrat.fayzulla@gmail.com>
2025-03-17 11:32:12 +0500

    is_prime and input output template added

---
+++ template.py
@@ -0,0 +1,12 @@
+import sys
+
+t = int(sys.stdin.readline().strip())
+results = []
+for _ in range(t):
+
+    n = int(sys.stdin.readline().strip())
+    s = sys.stdin.readline().strip()
+    results.append()
+
+
+sys.stdout.write("\n".join(results) + "\n")
