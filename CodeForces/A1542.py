cf888f1ff6cc8393f361d3661c67a871a2b04b99
s.fayzullaev <shukhrat.fayzulla@gmail.com>
2025-03-22 23:37:21 +0500

    A1542 solved

---
+++ CodeForces/A1542.py
@@ -0,0 +1,17 @@
+import sys
+
+t = int(sys.stdin.readline().strip())
+results = []
+for _ in range(t):
+
+    n = int(sys.stdin.readline().strip())
+    a = list(map(int, sys.stdin.readline().strip().split()))
+    
+    evens = 0
+    for i in a:
+        if i % 2 == 0:
+            evens += 1
+            
+    results.append("YES" if len(a) / 2 == evens else "NO")
+
+sys.stdout.write("\n".join(results) + "\n")
