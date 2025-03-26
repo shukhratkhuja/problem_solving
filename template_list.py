5329b78bdbf95fe799bb9eb5f59d763d51588d14
shukhratkhuja <shukhrat.fayzulla@gmail.com>
2025-03-26 17:01:57 +0500

    template list added

---
+++ template_list.py
@@ -0,0 +1,12 @@
+import sys
+
+t = int(sys.stdin.readline().strip())
+results = []
+for _ in range(t):
+
+    n = int(sys.stdin.readline().strip())
+    a = list(map(int, sys.stdin.readline().strip().split()))
+    results.append()
+
+
+sys.stdout.write("\n".join(results) + "\n")
