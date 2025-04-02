4fff93b1cda3f7654f3c547e360e91fec3386d43
shukhratkhuja <shukhrat.fayzulla@gmail.com>
2025-04-02 18:02:12 +0500

    A1325 solved

---
+++ CodeForces/A1325.py
@@ -0,0 +1,12 @@
+import sys
+
+t = int(sys.stdin.readline().strip())
+results = []
+for _ in range(t):
+
+    n = int(sys.stdin.readline().strip())
+    sys.stdout.write("\n")
+    sys.stdout.write("1 " + str(n-1))
+    
+sys.stdout.write("\n")
+
