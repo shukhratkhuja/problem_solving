e16e2eb863e294ea68678914ffc24c66fb75867a
shukhratkhuja <shukhrat.fayzulla@gmail.com>
2025-04-02 16:42:54 +0500

    new template added

---
+++ template_print_separetely.py
@@ -0,0 +1,11 @@
+import sys
+
+t = int(sys.stdin.readline().strip())
+for _ in range(t):
+
+    a, b, c = map(int, sys.stdin.readline().strip().split())
+    
+    sys.stdout.write("\n")
+    
+sys.stdout.write("\n")
+
